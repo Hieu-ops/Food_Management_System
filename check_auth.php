b2b9085c7948499fc3495de2d25f594d0c634bac
decodervn <109862173+decodervn@users.noreply.github.com>
2026-01-02 10:34:48 +0700

    Update check_auth.php

--- check_auth.php
+++ check_auth.php
@@ -1,22 +1,30 @@
 <?php
 include("connection.php");
 
-if (!isset($_COOKIE["auth_token"])) {
+$redirect = function () {
     header("Location: login.php");
     exit();
+};
+
+if (!isset($_COOKIE["auth_token"])) {
+    $redirect();
 }
 
 $token = $_COOKIE["auth_token"];
 
-$stmt = $conn->prepare("SELECT * FROM user WHERE auth_token = ? LIMIT 1");
+$stmt = $conn->prepare("SELECT * FROM users WHERE auth_token = ? LIMIT 1");
+if (!$stmt) {
+    $redirect();
+}
+
 $stmt->bind_param("s", $token);
 $stmt->execute();
 $result = $stmt->get_result();
 
-if ($result->num_rows !== 1) {
-    header("Location: login.php");
-    exit();
+if (!$result || $result->num_rows !== 1) {
+    $redirect();
 }
 
 $current_user = $result->fetch_assoc();
+$stmt->close();
 ?>
